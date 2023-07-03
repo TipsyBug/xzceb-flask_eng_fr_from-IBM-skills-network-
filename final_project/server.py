175a79a9de7f220654b80d0ea49e58193b6a0237
TipsyBug <tipsybug@gmail.com>
2023-07-03 14:56:00 -0400

    Final changes

--- final_project/server.py
+++ final_project/server.py
@@ -4,13 +4,13 @@ import json
 
 app = Flask("Web Translator")
 
-@app.route("/final_project/machinetranslation/englishToFrench", methods=['GET'])
+@app.route("/englishToFrench", methods=['GET'])
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
     french_text = translator.english_to_french(textToTranslate)
     return "Translated text to French: " + french_text
 
-@app.route("/final_project/machinetranslation/frenchToEnglish", methods=['GET'])
+@app.route("/frenchToEnglish", methods=['GET'])
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
     english_text = translator.french_to_english(textToTranslate)
