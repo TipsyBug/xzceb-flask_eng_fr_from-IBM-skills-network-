f7576e6424fefb89d6f85cc145b2251ad4f9259b
TipsyBug <tipsybug@gmail.com>
2023-07-03 11:53:06 -0400

    Temporary changes

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -14,7 +14,7 @@ def english_to_french(english_text):
     and returns the French translation as output.
     """
 
-    french_text = MyMemoryTranslator(source='en', target='fr').translate(english_text)
+    french_text = MyMemoryTranslator(source='en-EN', target='fr-FR').translate(english_text)
     return french_text
 
 def french_to_english(french_text):
