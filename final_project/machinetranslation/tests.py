f6d61408a4c499df30e422f5aed7286c6c78f7a7
TipsyBug <tipsybug@gmail.com>
2023-07-03 09:30:48 -0400

    Temporary changes

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -1,14 +1,19 @@
 import unittest
-from translator import english_to_french
-from translator import french_to_english
+from translator import english_to_french, french_to_english
 
 class MyTest(unittest.TestCase):
 
     def test_e2f(self):
-        self.assertEqual(english_to_french("Hello"), "Bonjour")
+        self.assertEqual(english_to_french("hello"), "bonjour")
+    
+    def test_e2f2(self):
+        self.assertNotEqual(english_to_french('Hello'), 'Hello')
 
     def test_f2e(self):
-        self.assertEqual(french_to_english("Bonjour"), "Hello")
+        self.assertEqual(french_to_english("bonjour"), "hello")
+
+    def test_f2e2(self):
+        self.assertNotEqual(french_to_english('Bonjour'), 'Bonjour')
 
 if __name__ == "__main__":
     unittest.main()
